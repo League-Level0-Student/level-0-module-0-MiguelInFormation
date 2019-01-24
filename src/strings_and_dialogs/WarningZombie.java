8367b3be028700d43209c8a456d9f62c4c0f94d4
ilabstaff <ilabstaff@172.16.116.73>
2019-01-23 19:24:24 -0800

    January 23rd 2019

---
+++ src/strings_and_dialogs/WarningZombie.java
@@ -0,0 +1,21 @@
+package strings_and_dialogs;
+
+import javax.swing.JOptionPane;
+
+public class WarningZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "he wake up");
+	JOptionPane.showMessageDialog(null, "he then gets out of bed");
+	JOptionPane.showMessageDialog(null, "he then goes to the kitchen to get himself a bowl");
+	JOptionPane.showMessageDialog(null, "after he gets himself a bowl..");
+	JOptionPane.showMessageDialog(null, "he gets his favourite cereal and serves himself");
+	JOptionPane.showMessageDialog(null, "his morning ends.");
+	JOptionPane.showMessageDialog(null, "he drives off to work");
+	JOptionPane.showMessageDialog(null, "the clock tower strikes 12");
+	JOptionPane.showMessageDialog(null, "it is no longer the morning. it is now noon.");
+	
+	
+	
+	
+}
+}
